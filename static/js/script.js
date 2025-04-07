af747e0035bbe08d2e301bdb8085afbbe33b68c6
William Lee <williampaullee@gmail.com>
2025-04-06 20:29:27 -0400

    final draft!!

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () 
+{
   // Get the current page URL 
   let currentPage = window.location.pathname.split("/").pop() || "index.html";
 
   // Select the navigation links
-  let links = document.querySelectorAll("nav ul li a");
+  const links = document.querySelectorAll("nav ul li a");
 
-  // Loop through to match the navigation link with the page we are on
-  for (let i = 0; i < links.length; i++) {
-    let linkPage = links[i].getAttribute("href").split("/").pop();
 
-    if (currentPage === linkPage) {
-      links[i].classList.add("active");
-      break; 
+  // Apply the active class ONLY when user first starts interacting with the page
+  // This ensures no button is highlighted when the page is first loaded
+  let navigationBarInteraction = sessionStorage.getItem("checkClicked") === "true";
+  if (navigationBarInteraction) 
+  {
+    // Loop through to match the navigation link with the page we are on
+    for (let i = 0; i < links.length; i++) 
+    {
+      let linkPage = links[i].getAttribute("href").split("/").pop();
+
+      if (currentPage === linkPage) 
+      {
+        links[i].classList.add("active");
+        break; 
+      }
     }
   }
+
+  links.forEach(link => {
+    link.addEventListener("click", function () {
+      sessionStorage.setItem("checkClicked", "true"); 
+    });
+  });
 });
