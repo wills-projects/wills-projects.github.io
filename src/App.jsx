b90ae8169c67334767ea07f96ae651570aebfb60
William <williampaullee@gmail.com>
2025-04-24 01:08:05 -0400

    Launch React project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { HashRouter, Routes, Route  } from 'react-router-dom'
+import React from "react";
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Experience from './pages/Experience'
