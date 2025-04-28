9f5da847d12b876b3624baac6a6d3d98025688f0
William Lee <williampaullee@gmail.com>
2025-04-28 13:21:58 -0400

    fixed mobile responsiveness and removed redundant javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
@@ -7,8 +8,70 @@ import Leadership from './pages/Leadership'
 import About from './pages/About'
 
 function App() {
+  useEffect(() => {
+    const logos = document.querySelectorAll('.background_div');
+    
+    const backgroundColor = [ '#A3B18A', '#8D9F6C', '#5C6E3D', '#8D9F6C', '#94A684'];
+    
+    
+    for (let i = 0; i < logos.length; i++) {
+        logos[i].style.setProperty('--i', i);
+        logos[i].style.setProperty('--d', Math.random() * 8);
+        logos[i].style.setProperty('--a', Math.random() * 8 + 4);
+        logos[i].style.setProperty('--y', Math.floor(Math.random() * 100));
+    
+        const randColor = backgroundColor[Math.floor(Math.random() * backgroundColor.length)];
+        logos[i].style.setProperty('--color', randColor);
+        }
+    }, []); 
+
   return(
     <>
+      <section className="background">
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+        <div className="background_div"></div>
+      </section>
       <HashRouter>
         <Routes>
           <Route index element={<Home/>}/>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,6 @@ function Footer()
     <footer class="footing">
       <ul class="footer_icons">
         <li>
-          <a href="/#/home"
-            ><img src="/home icon.png" alt="Home Icon" />
-            <p>Home</p></a>
-        </li>
-        <li>
-          <a href="/#/about"
-            ><img
-              src="/about me icon.png"
-              alt="Info/About Me Icon"
-            />
-            <p>About Me</p></a>
-        </li>
-        <li>
           <a href="mailto:williampaullee@gmail.com"
             ><img
               src="/contact me icon.png"
